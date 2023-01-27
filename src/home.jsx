422f0bbb35c2706d4ac0c954af81f1a4ae22649b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-27 14:56:40 +0100

    add file

--- src/home.jsx
+++ src/home.jsx
@@ -6,8 +6,8 @@ export const HomePage = ({ data }) => {
         <div>
             <h1>DWTC - IA FUTURE</h1>
             <p>Add company</p>
-            <Form/>
-            <h2>Liste des exposants</h2>
+            {/* <Form/> */}
+            <h2>List of companies</h2>
             <ul className="list">
                 {data.map((project, index) => <li key={index}><Link to={project.link}>{project.name}</Link></li>)}
             </ul>
