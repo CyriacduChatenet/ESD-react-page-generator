04f3bfcca4129456c35a4fbb41d60ff86ffdfab7
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-27 15:01:28 +0100

    add files and css

--- src/page.jsx
+++ src/page.jsx
@@ -10,7 +10,7 @@ export const Page = ({ background, name, logo, primary_color, secondary_color, d
             </div>
             <section className="main">
                 <div className="left">
-                    <h2>Enjeux</h2>
+                    <h2>AI challenges</h2>
                     <p>AI faces challenges such as safety, ethics, and societal benefits. By 2030, it may have a significant impact on various aspects of life like transportation, healthcare, education, and economy, but also negative consequences such as social inequality. It is important to consider both benefits and negative consequences.</p>
                     <img src="" alt="" />
                     <p>In addition to the challenges and impact already mentioned, another important consideration for the development and use of AI is the issue of regulation. As AI becomes more advanced and more widely used, it will be crucial to have clear and effective regulations in place to govern its development, deployment, and use. This could include regulations around data privacy, transparency, and accountability for AI systems, as well as guidelines for the safe and ethical use of AI in various industries.</p>
